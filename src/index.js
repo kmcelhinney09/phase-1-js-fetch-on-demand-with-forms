222c0bda05293e3def780f38679830fc8ab02068
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-10-24 17:01:23 -0700

    commit tree 8d6fa77a4e7fe6b859ae099c22dd23a58279306c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', (e) =>{
+      e.preventDefault();
+      const fetchData = 'http://localhost:3000/movies/' + e.target.searchByID.value
+      fetchRequest(fetchData);
+      inputForm.reset()
+  })
   
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function fetchRequest(fetchData){
+    return fetch(fetchData)
+    .then(response => response.json())
+    .then(data => {
+        updateMovieData(data.title, data.summary)
+    })
+}
+
+function updateMovieData(movieTitle = 'Title',movieSummary = "Summary"){
+    const title = document.querySelector('section#movieDetails h4');
+    const summary = document.querySelector('section#movieDetails p')
+
+    title.textContent = movieTitle
+    summary.textContent = movieSummary
+}
